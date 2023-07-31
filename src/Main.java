1332bb4d3d6164bf3a29d3f7c95db4f0260ad249
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-07-31 03:17:48 +0100

    Changement du LookAndFeel grâce à FlatLaf 3.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.formdev.flatlaf.FlatLightLaf;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 
 	try {
-		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		FlatLightLaf.setup();
+		UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");
+		//UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 	} catch (ClassNotFoundException | InstantiationException
 			| IllegalAccessException | UnsupportedLookAndFeelException e) {
